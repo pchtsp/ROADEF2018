0a339bf67a0a929960749d734ae79f27a00b0a36
pchtsp <franco.peschiera@gmail.com>
2018-06-22 10:26:14 +0200

    update the params file.

--- python/package/params.py
+++ python/package/params.py
@@ -22,12 +22,12 @@ cut_level_next_o = {
 }
 
 OPTIONS = {
-    'timeLimit': 1800
+    'timeLimit': 1200
     , 'gap': 0.1
     , 'solver': "HEUR"  # CPLEXPY, CPLEX_CMD, GUROBI, HEUR
     , 'integer': False
     , 'path': PATHS['experiments']
-    , 'case_name': 'A8'
+    , 'case_name': 'A2'
     , 'max_plates': 10
     , 'max_width': 6000//4
     , 'max_items': 15
@@ -37,10 +37,10 @@ OPTIONS = {
     # , 'heur_weights': {'space': 10, 'seq': 100000, 'defects': 1000}
     , 'heur_params': {'main_iter': 5, 'max_iter': 1000,
                       'temperature': 8000, 'try_rotation': False,
-                      'max_candidates': 10, 'extra_jumbos': 2,
-                      'cooling_rate': 0.02,
+                      'max_candidates': 10, 'extra_jumbos': 1,
+                      'cooling_rate': 0.01,
                       'change_first': False, 'tolerance': None}
-    , 'debug': False
+    , 'debug': True
     # putting a tolerance on the heuristic seems horrible for finding good solutions.
     # putting change_first is also a very bad idea.
     # , 'cluster_tolerance': 50
