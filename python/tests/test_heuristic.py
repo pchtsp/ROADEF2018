d0870367c48c02af535b3797cf7add36848d0a8b
pchtsp <franco.peschiera@gmail.com>
2018-07-11 11:45:45 +0200

    test example and list of things to test

--- python/tests/test_heuristic.py
+++ python/tests/test_heuristic.py
@@ -6,3 +6,18 @@
 #         calc = Calculator()
 #         result = calc.add(2, 2)
 #         self.assertEqual(4, result)
+
+# test checking defects when swapping.
+# test sequence when swapping.
+# test correct swapping under several conditions
+# test
+
+"""
+types of swapping cases:
+* same plate siblings
+* same plate, not siblings
+* different plate same level
+* different plate different level
+*
+
+"""
