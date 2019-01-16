ef70fee52799592c1f5a128e002d8aebafb76e80
pchtsp <franco.peschiera@gmail.com>
2019-01-16 17:22:40 +0100

    corrected examples

--- bash/examples.sh
+++ bash/examples.sh
@@ -2,7 +2,7 @@ python3 Documents/projects/ROADEF2018/python/scripts/exec.py --all-cases --path-
 
 python3 Documents/projects/ROADEF2018/python/scripts/exec.py --case-name A14 --path-root /home/pchtsp/Documents/projects/ROADEF2018/ --path-results /home/pchtsp/Dropbox/ROADEF2018/ --results-dir hp_20180916_venv --no-graph --extra-jumbos 0 --time-limit 600 --num-process 1 --heur-remake '{"iterations_initial": 1000, "iterations_remake": 100}'
 
-python3 Documents/projects/ROADEF2018/python/scripts/exec.py --all-cases --path-root /home/disc/f.peschiera/Documents/projects/ROADEF2018/ --path-results /home/disc/f.peschiera/Documents/projects/ROADEF2018/results/ --results-dir prise_20180918_venv --no-graph --extra-jumbos 0 --time-limit 3600 -s 500 --num-process 12 --heur-remake '{"iterations_initial": 1000, "iterations_remake": 100}' '{"cooling_rate": 0.005}' & 
+python3 Documents/projects/ROADEF2018/python/scripts/exec.py --all-cases --path-root /home/disc/f.peschiera/Documents/projects/ROADEF2018/ --path-results /home/disc/f.peschiera/Documents/projects/ROADEF2018/results/ --results-dir prise_20180918_venv --no-graph --extra-jumbos 0 --time-limit 3600 -s 500 --num-process 12 --heur-remake '{"iterations_initial": 1000, "iterations_remake": 100}' --heur-params '{"cooling_rate": 0.005}' & 
 
 python scripts/exec.py --all-cases --path-root ./../ --path-results ./../results/ --results-dir test -t 180 & 
 
@@ -10,4 +10,8 @@ python scripts/exec.py -p ./../resources/data/dataset_A/A3 -o ./../resources/che
 
 python scripts/exec.py --all-cases --path-root ./../ --path-results ./../results/ --results-dir hp_20181123 --time-limit 3600 & 
 
-python python/scripts/exec.py --all-cases --path-root ./ --path-results /home/pchtsp/Dropbox/ROADEF2018/ --results-dir hp_20181126 --time-limit 3600 & > log.txt
+python python/scripts/exec.py --all-cases --path-root ./ --path-results /home/pchtsp/Dropbox/ROADEF2018/ --results-dir hp_20181126 --time-limit 3600 > log.txt &
+
+ python python/scripts/exec.py --all-cases --path-root ./ --path-results /home/pchtsp/Dropbox/ROADEF2018/ --results-dir hp_20181210 --time-limit 3600 --data-set B > log.txt &
+
+python python/scripts/exec.py --all-cases --path-root ./ --path-results /home/pchtsp/Dropbox/ROADEF2018/ --results-dir hp_20181212 --time-limit 3600 --data-set A --heur-remake "{\"iterations_initial\": 1000, \"iterations_remake\": 100, \"num_trees\": [0.20, 0.20, 0.20, 0.20, 0.20]}" --heur-params  "{\"rotation_probs\": [0.5, 0.5, 0, 0]}" > log.txt & 
